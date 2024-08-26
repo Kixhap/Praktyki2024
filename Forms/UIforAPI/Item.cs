7c37be908790caba32d4b13531348b7e61f3bd99
Kixhap <69913222+Kixhap@users.noreply.github.com>
2024-08-26 13:27:21 +0200

    cleanup

--- Forms/UIforAPI/Item.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace UIforAPI.Models
-{
-    public class Item
-    {
-        public int Id { get; set; }
-        public string Name { get; set; }
-        public string Description { get; set; }
-    }
-}
--- Forms/UIforAPI/UIforAPI/obj/Debug/net8.0-windows/UIforAPI.AssemblyInfo.cs
+++ Forms/UIforAPI/UIforAPI/obj/Debug/net8.0-windows/UIforAPI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("UIforAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+177e8c13c5e091bd34883d5effed2e205a62ccbe")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+919dcf19bd38ebb7460bff5a20302e1a1dab991d")]
 [assembly: System.Reflection.AssemblyProductAttribute("UIforAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("UIforAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
